ae311f8391e5d91fd3f0e298306c7e81bb8ecca8
Samuel <samuellm@live.com>
2015-08-18 17:16:49 -0300

    Adicionado menu com lista

--- pizzaria/pizza/views.py
+++ pizzaria/pizza/views.py
@@ -3,9 +3,6 @@ from django.http import HttpResponse
 from .models import Pizza
 
 def menu(request):
-    pizzas = Pizza.object.all()
-    result = ''
-    for pizza in pizzas:
-        result += pizza.name
-        result += '<br>'
-    return HttpResponse(result)
+    pizzas = Pizza.objects.all()
+    context = {'pizzas': pizzas}
+    return render(request, 'pizza/menu.html', context)
