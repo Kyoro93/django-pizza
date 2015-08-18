41624a054ca17928cbbb270128e751ac94afa9da
Samuel <samuellm@live.com>
2015-08-18 16:54:55 -0300

    modificado o views.py

--- pizzaria/pizza/views.py
+++ pizzaria/pizza/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pizza
+
 def menu(request):
-    return HttpResponse('Hello World')
+    pizzas = Pizza.object.all()
+    result = ''
+    for pizza in pizzas:
+        result += pizza.name
+        result += '<br>'
+    return HttpResponse(result)
