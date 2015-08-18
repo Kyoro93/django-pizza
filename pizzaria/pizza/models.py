9277410cdb51471ab658f4697ab7e69b8bc61a18
Samuel <samuellm@live.com>
2015-08-18 15:05:05 -0300

    atualizado models Pizza

--- pizzaria/pizza/models.py
+++ pizzaria/pizza/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Pizza(models.Model):
-    name = models.CharField(max_lenght=120)
-    price = models.DecimalField(decimal_places=2)
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
     ingredients = models.TextField()
+
+    def __unicode__(self):
+        return self.name
