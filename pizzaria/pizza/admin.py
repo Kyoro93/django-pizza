db6cc7c51619b6201aa256b67a1530bb550b7a73
Samuel <samuellm@live.com>
2015-08-18 15:40:52 -0300

    adicionado informações de lista

--- pizzaria/pizza/admin.py
+++ pizzaria/pizza/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Pizza
 
-admin.site.register(Pizza)
+
+class PizzaAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'ingredients','price']
+    list_display = ['name', 'ingredients', 'price']
+
+admin.site.register(Pizza, PizzaAdmin)
